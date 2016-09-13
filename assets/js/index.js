548a23d6fe47cbf481efc4c2a5fb113aed3d971e
Arleigh Mitchell <arleighmitchell@gmail.com>
2016-09-13 14:04:38 -0700

    inital commit

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+var people = 5;//golbal variabel
+
+function number_of_people(people) {
+	console.log(people + 2);/*tells you what to do to the item passed
+	that's passed through when the function is called*/
+}
+
+number_of_people(10);//function is called,(10) is whats being passed thru
+
+console.log(people);//calling the global funciton
