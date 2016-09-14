a70bfd85a60631bc2299799f449a56457d52e2b2
Arleigh Mitchell <arleighmitchell@gmail.com>
2016-09-13 22:18:08 -0700

    edit password and username qualifications

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,31 @@ function showDiv2() {
    document.getElementById('Div2').style.display = "block";
    document.getElementById('Div1').style.display = "none";
 }
+
+
+document.getElementById("submit").addEventListener = ("click", function(event) {
+	checkPassword();
+	checkUsername();
+}
+
+function checkPassword() {
+	var password = document.getElementById("password").value;
+	var ok = true;
+	if (password != 12345678) {
+		ok = false;
+		alert("Password is Wrong!!!")
+	} else {
+		alert("Password Right!!!")
+	}
+	return ok;
+	}
+
+function checkUsername() {
+	var username = /\d/.test(document.getElementById("username").value);
+	if (username == false) {
+		alert("Username must contain at least one digit!!")
+	} else {
+		alert("Username is Correct!!")
+	}
+	return ok;
+}
